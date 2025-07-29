63b63ef2b715c3a95e9739bb66d5e6c140c7a82f
VictorDSilva <victordomingos91@gmail.com>
2025-07-29 19:36:18 -0300

    Changin nat value

--- terraform/main.tf
+++ terraform/main.tf
@@ -91,9 +91,9 @@ resource "aws_subnet" "private" {
   }
 }
 
-# NAT Gateway - ALTERADO: Usando apenas 1 para economizar EIPs
+
 resource "aws_eip" "nat" {
-  count = 2
+  count = 1  
 
   domain     = "vpc"
   depends_on = [aws_internet_gateway.main]
@@ -104,10 +104,10 @@ resource "aws_eip" "nat" {
 }
 
 resource "aws_nat_gateway" "main" {
-  count = 2 
+  count = 1
 
   allocation_id = aws_eip.nat[0].id
-  subnet_id     = aws_subnet.public[0].id  # Usando apenas a primeira subnet pública
+  subnet_id     = aws_subnet.public[0].id
 
   tags = {
     Name = "${var.project_name}-${var.environment}-nat-gateway"
