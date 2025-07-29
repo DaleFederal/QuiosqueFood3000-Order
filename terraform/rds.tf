515494fcb2c0c69ee69e1be087a7d5201a517364
VictorDSilva <victordomingos91@gmail.com>
2025-07-29 11:59:11 -0300

    Ajustando format rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -45,31 +45,31 @@ resource "aws_db_parameter_group" "postgres" {
 
 # RDS Instance
 resource "aws_db_instance" "postgres" {
-  identifier             = "${lower(var.project_name)}-${var.environment}-db"
-  allocated_storage      = 20
-  max_allocated_storage  = 100
-  storage_type           = "gp2"
-  engine                 = "postgres"
-  engine_version         = "15.8"
-  instance_class         = "db.t3.micro"
-  
+  identifier            = "${lower(var.project_name)}-${var.environment}-db"
+  allocated_storage     = 20
+  max_allocated_storage = 100
+  storage_type          = "gp2"
+  engine                = "postgres"
+  engine_version        = "15.8"
+  instance_class        = "db.t3.micro"
+
   db_name  = "QuiosqueFood3000OrderDB"
   username = "postgres"
   password = var.db_password
-  
+
   vpc_security_group_ids = [aws_security_group.rds.id]
   db_subnet_group_name   = aws_db_subnet_group.main.name
   parameter_group_name   = aws_db_parameter_group.postgres.name
-  
+
   backup_retention_period = 7
-  backup_window          = "03:00-04:00"
-  maintenance_window     = "sun:04:00-sun:05:00"
-  
+  backup_window           = "03:00-04:00"
+  maintenance_window      = "sun:04:00-sun:05:00"
+
   skip_final_snapshot = true
   deletion_protection = false
-  
+
   publicly_accessible = false
-  
+
   tags = {
     Name = "${var.project_name}-${var.environment}-postgres"
   }
