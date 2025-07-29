e499c7860eb98a17bab8d25ba08d3c8712e9eaa9
VictorDSilva <victordomingos91@gmail.com>
2025-07-28 22:52:57 -0300

    Não subi

--- terraform/main.tf
+++ terraform/main.tf
@@ -219,7 +219,7 @@ resource "aws_security_group" "ecs_tasks" {
 
 # Application Load Balancer
 resource "aws_lb" "main" {
-  name               = "QF3K-${substr(var.environment, 0, 4)}-alb"
+  name               = "VD0S-${substr(var.environment, 0, 4)}-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb.id]
@@ -228,12 +228,12 @@ resource "aws_lb" "main" {
   enable_deletion_protection = false
 
   tags = {
-    Name = "QF3K-${substr(var.environment, 0, 4)}-alb"
+    Name = "VD0S-${substr(var.environment, 0, 4)}-alb"
   }
 }
 
 resource "aws_lb_target_group" "app" {
-  name        = "QF3K-${substr(var.environment, 0, 4)}-tg"
+  name        = "VD0S-${substr(var.environment, 0, 4)}-tg"
   port        = var.container_port
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
